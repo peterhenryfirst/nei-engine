cfaa1ac9685aa8beba79177748ec68b4778937a6
Pedro Pereira <pereira.pedro.h@gmail.com>
2014-09-05 14:31:32 +0200

    Added first dependency with test properties needed

--- nei/test/app/BaseApp.js
+++ nei/test/app/BaseApp.js
@@ -8,9 +8,13 @@ var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
 describe('Base App tests', function() {
-	it('should exist BaseApp', function() {
+	it('should exist BaseApp with basic properties', function() {
 		should.exist(base);
 		base.should.be.an('object');
+
+		expect(base).to.have.property('init').and.to.be.a('function');
+		expect(base).to.have.property('release').and.to.be.a('function');
+
 	});
 	
 	it('should exist functions run and tick', function() {
@@ -20,7 +24,13 @@ describe('Base App tests', function() {
 	});
 	
 	it('should exist properties to manage the state machine', function() {
-		should.exist(base);
-		base.should.be.an('object');
+
+		expect(base).to.have.property('fsm').and.to.be.a('object');
+
+		expect(base).to.have.property('releaseAllStates').and.to.be.a('function');
+		expect(base).to.have.property('addState').and.to.be.a('function');
+		expect(base).to.have.property('setState').and.to.be.a('function');
+		expect(base).to.have.property('changeState').and.to.be.a('function');
+		
 	});
 });
