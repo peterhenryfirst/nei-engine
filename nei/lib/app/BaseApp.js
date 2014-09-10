37770e23d5297370e938a5ab69dd668d8a12ea4b
Peter Pereira <pereira.pedro.h@gmail.com>
2014-09-11 00:25:23 +0200

    Revert "Revert "More tests to BaseApp""
    This reverts commit 19bcd5d9b1ea3ca77cca6e9c654621867779ca16.

--- nei/lib/app/BaseApp.js
+++ nei/lib/app/BaseApp.js
@@ -5,16 +5,53 @@ var baseApp = ( function (baseSubModule) {
 	
 	//private properties
 	
+	var _initialized = false;
+	
+	function init() {
+		
+		if (_initialized) {
+			throw new Error('The application was initilized previously');
+		}
+		_initialized = true;
+	}
+	
+	function release() {
+		console.log('RELEASE: ' + _initialized);
+		if (!_initialized) {
+			console.log('RELEASE2');
+			throw new Error('The application was not initilized previously');
+		}
+		_initialized = false;
+	}
+	
+	function isInitiated() {
+		return _initialized;
+	}
+	
 	function run() {
 	}
 	
 	function tick() {
 	}
 	
+	function setState() {
+	}
+	
+	function changeState() {
+	}
+	
 	//public properties
 	baseSubModule = {
+		
+		init: init,
+		release: release,
+		isInitiated: isInitiated,
+		
 		run: run,
-		tick: tick
+		tick: tick,
+		
+		setState: setState,
+		changeState: changeState
 	};
 	
 	module.exports = baseSubModule;
