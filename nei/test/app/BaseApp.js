19bcd5d9b1ea3ca77cca6e9c654621867779ca16
Peter Pereira <pereira.pedro.h@gmail.com>
2014-09-11 00:25:15 +0200

    Revert "More tests to BaseApp"
    This reverts commit f3ab65e3771183373a6417a54ecf2161300413cc.

--- nei/test/app/BaseApp.js
+++ nei/test/app/BaseApp.js
@@ -1,3 +1,5 @@
+var base = require('../../lib/app/BaseApp');
+
 var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
@@ -6,77 +8,13 @@ var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
 describe('Base App tests', function() {
-	
-	var base = require('../../lib/app/BaseApp');
-	
-	beforeEach(function(done) {
-		done();
-	});
-	
-	afterEach(function(done) {
-		
-		if (base.isInitiated()) {
-			base.release();
-		}
-		
-		done();
-	});
-	
 	it('should exist BaseApp with basic properties', function() {
 		should.exist(base);
 		base.should.be.an('object');
 
 		expect(base).to.have.property('init').and.to.be.a('function');
 		expect(base).to.have.property('release').and.to.be.a('function');
-		
-		expect(base).to.have.property('isInitiated').and.to.be.a('function');
 
-	});
-	
-	it('should get false from isInitiated method before any call', function() {
-		expect(base.isInitiated()).to.be.false;
-	});
-	
-	it('should throw error when try to initialize twice', function() {
-		/*
-		var stub = sinon.stub();
-		
-		base.init = stub;
-		
-		base.init();
-		
-		expect(stub).to.have.been.called;
-		/**/
-		
-		var spy = sinon.spy(base, 'init');
-		
-		base.init();
-		
-		try {
-			base.init();
-		} catch (e) {
-		}
-		
-		expect(spy).to.have.been.calledTwice;
-		
-		spy.getCall(1).should.have.thrown();
-		
-		spy.restore();
-	});
-	
-	it('should throw error when try to call release before init', function() {
-		
-		var spy = sinon.spy(base, 'release');
-		
-		try {
-			base.release();
-		} catch (e) {
-		}
-		
-		expect(spy).to.have.been.called;
-		
-		spy.should.have.thrown();
-		
 	});
 	
 	it('should exist functions run and tick', function() {
@@ -87,10 +25,10 @@ describe('Base App tests', function() {
 	
 	it('should exist properties to manage the state machine', function() {
 
-		//expect(base).to.have.property('fsm').and.to.be.a('object');
+		expect(base).to.have.property('fsm').and.to.be.a('object');
 
-		//expect(base).to.have.property('releaseAllStates').and.to.be.a('function');
-		//expect(base).to.have.property('addState').and.to.be.a('function');
+		expect(base).to.have.property('releaseAllStates').and.to.be.a('function');
+		expect(base).to.have.property('addState').and.to.be.a('function');
 		expect(base).to.have.property('setState').and.to.be.a('function');
 		expect(base).to.have.property('changeState').and.to.be.a('function');
 		
